d29bae3413114de6b49c32442079ad10ed3a0cf9
raviteja-reddy-guntaka <guntaka.ravitejareddy@gmail.com>
2025-01-12 21:26:05 -0800

    Updated the unit test

--- sentiment-analysis-plugin/src/test/java/org/opensearch/plugins/sentiment/SentimentProcessorTest.java
+++ sentiment-analysis-plugin/src/test/java/org/opensearch/plugins/sentiment/SentimentProcessorTest.java
@@ -1,16 +1,17 @@
 package org.opensearch.plugins.sentiment;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SentimentProcessorTest {
 
-    // @Test
-    // void testAnalyzeSentiment() {
-    //     SentimentProcessor processor = new SentimentProcessor("test", "test description", null);
-    //     double score = processor.analyzeSentiment("This is a great product!");
-    //     System.out.println(score);
-    //     assertTrue(score > 0, "Positive sentiment should have a score greater than 0");
-    // }
+    @Test
+    void testAnalyzeSentiment() {
+        SentimentProcessor processor = new SentimentProcessor("test", "test description", null);
+        Map<String, Object> result = processor.analyzeSentiment("This is a great product!");
+        assertTrue((Double) result.get("score") > 0, "Positive sentiment should have a score greater than 0");
+    }
 
 }
